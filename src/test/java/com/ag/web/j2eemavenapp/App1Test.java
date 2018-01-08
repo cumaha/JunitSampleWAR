1929969b712ad52e18f392c3ba8a94cea06f86e3
Srivastava <Ashish.Srivastava@sprint.com>
2018-01-08 09:36:47 -0600

    delete redundant module

--- src/test/java/com/ag/web/j2eemavenapp/App1Test.java
+++ src/test/java/com/ag/web/j2eemavenapp/App1Test.java
@@ -5,12 +5,11 @@ import static org.junit.Assert.*;
 import org.junit.Test;
  public class App1Test {
   @Test
-	public static String test() {
+	public void test() {
+		//System.out.println("This is test");
 		App1 app1 = new App1();
 		assertEquals("This is APP1.java",app1.SampleApp());
-		return app1.SampleApp();
+		
 	}
 
  }
-
-
