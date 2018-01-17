2a0b2ff58b71d83677205a3ece58d6032cd4fda5
Ashish <wiz4hostdomains@gmail.com>
2018-01-17 00:21:00 -0600

    added Selenium module with Chrome for hostnames

--- src/main/java/mySeleniumPkg/LocatorConcept.java
+++ src/main/java/mySeleniumPkg/LocatorConcept.java
@@ -1,15 +1,28 @@
 package mySeleniumPkg;
 
+
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LocatorConcept {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+
+		InetAddress ips;
+		ips = InetAddress.getLocalHost();
+        String hostname = ips.getHostName();
+		System.out.println(hostname);
 		
-		//Chrome Browser
-		System.setProperty("webdriver.chrome.driver", "C:\\MyLab\\ToolsAndSoftwares\\selenium-java-3.8.1\\browserdrivers\\chromedriver_v2.32_32bit.exe");
+	//Chrome Browser
+        if ((hostname).equals("LAPTOP-0SGNH2UI")){ System.setProperty("webdriver.chrome.driver", "C:\\MyLab\\ToolsAndSoftwares\\selenium-java-3.8.1\\browserdrivers\\chromedriver_v2.32_32bit.exe");}
+        if ((hostname).equals("W17044768")){ System.setProperty("webdriver.chrome.driver", "/opt/packages/tcwarpropfiles/chromedriver_v2.32_linux64");}
+        if ((hostname).equals("jenkins")){ System.setProperty("webdriver.chrome.driver", "/opt/packages/tcwarpropfiles/chromedriver_v2.32_linux64");}
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://imgur.com/register");
 		
